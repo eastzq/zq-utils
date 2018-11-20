447b6f0e0c3d7942351c89998a6332d28aafd6ef
eastzq <eastzq@gmail.com>
2018-11-20 13:52:24 +0800

    新增接口

--- src/main/java/com/zq/utils/cli/intf/LocalCommandExecutor.java
+++ src/main/java/com/zq/utils/cli/intf/LocalCommandExecutor.java
@@ -1,7 +1,10 @@
 package com.zq.utils.cli.intf;
 
+import java.io.File;
+
 import com.zq.utils.cli.ExecuteResult;
 
 public interface LocalCommandExecutor {
     ExecuteResult executeCommand(String command, long timeout);
+    ExecuteResult executeCommand(String command, long timeout, String[] envp, File dir);
 }
